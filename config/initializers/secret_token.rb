fcaa46799187ab3cb390d48b436478e2ec903c67
Christian Schlensker <christian@cswebartisan.com>
2011-01-28 18:19:39 -0500

    bonjour

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rpress::Application.config.secret_token = '0c26bfaab215e4564602d27f526eb4d8e29b7f598c89f857fd68f3e3d87e299038aa30a571cb5d6ca20e0f30589dc7746730c2c877e25e2268744336176a4de0'
